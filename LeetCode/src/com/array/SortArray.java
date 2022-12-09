f6f0e277391728e184eacbea8daac57c9f9baf87
Nandita Ramakrishnan <nandita.ramakrishnan28@gmail.com>
2022-12-09 11:18:14 -0600

    Iterated over array code

--- LeetCode/src/com/array/SortArray.java
+++ LeetCode/src/com/array/SortArray.java
@@ -4,6 +4,12 @@ package com.array;
 public class SortArray{
   
   public static void main(String args[]){
+	  
+	  int a[] = {2,5,7};
+	  for(int i=0; i<a.length; i++) {
+		  System.out.println(a[i]);
+	  }
+	  
     System.out.println("This program sorts the input array"); 
   }
   
